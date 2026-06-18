7e02f52295a2d6d9be175635364858fd315bca3f
Spencer Feng <spencer.fengicloud.com>
2026-06-18 19:56:04 +1000

    feat: initial commit

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import stylistic from "@stylistic/eslint-plugin"
+import tseslint from "typescript-eslint"
+
+export default tseslint.config(
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      "@stylistic": stylistic,
+    },
+    rules: {
+      "@stylistic/quotes": ["error", "double"],
+      "@stylistic/semi": ["error", "never"],
+      "@stylistic/object-curly-spacing": ["error", "always"],
+      "@stylistic/array-bracket-spacing": ["error", "always"],
+      "@stylistic/comma-dangle": ["error", "always"],
+    },
+  },
+)
